b4383d585b371930110a63706ce5354fac25e591
ekl3m <eryk.klem03@gmail.com>
2025-07-26 11:41:45 +0200

    V0.3 - Project directory reorganization

---
+++ backend/src/main/java/com/nas_backend/NasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasBackendApplication.class, args);
+	}
+
+}
